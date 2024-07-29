467a3f37292668769a70e00ccbcab803b34cf504
Ruan Pablo <ruanpablodnzzz@gmail.com>
2024-07-29 11:22:53 -0300

    customizing buttons

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+        <h2 class="text-lg font-semibold leading-tight text-gray-800">
             {{ __('Clients') }}
         </h2>
     </x-slot>
     <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8  ">
             <form method="POST" action="{{ route('clients.store') }}">
                 @csrf
 
