c0f016091053e25b9fe232e5e41cb50a1a24d362
Ruan Pablo <ruanpablodnzzz@gmail.com>
2024-08-08 15:04:21 -0300

    add table component

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -2,55 +2,31 @@
 
 
     <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="px-4 sm:px-6 lg:px-8">
 
                 <x-heading
                     title="Client"
                     description="A list of all the clients."
                     btn-label="Add Client"
-                    :route=" route('clients.create')"
-                />
+                    :route= " route('clients.create') " />
 
+                <div class="w-full overflow-hidden md:rounded-lg">
+                    <livewire:table
+                        resource="Client"
+                        :columns="[
+                        ['label' => 'Client', 'column' => 'user.name'],
+                        ['label' => 'Email','column' => 'user.email'],
+                        ['label' => 'City', 'column' => 'address.city'],
+                        ['label' => 'State', 'column' => 'address.state'],
+                    ]
+                    "
+                        edit="clients.edit"
+                        delete="clients.destroy"
+                    />
+                </div>
 
-            <div class="max-w-2xl overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                <table class="divide-y divide-gray-300">
-                    <thead class="bg-gray-50">
-                    <tr>
-                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">City</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">State</th>
-                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                            <span class="sr-only">Edit</span>
-                        </th>
-                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                            <span class="sr-only">Delete</span>
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                    @foreach ($clients as $client)
-                        <tr>
-                            <td class="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6">{{ $client->user->name }}</td>
-                            <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $client->user->email }}</td>
-                            <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $client->address->city }}</td>
-                            <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $client->address->state }}</td>
-                            <td class="relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6">
-                                <a href="{{ route('clients.edit', $client->id) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                            </td>
-                            <td class="relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6">
-                                delete
-                            </td>
-                        </tr>
-                    @endforeach
-                    <!-- More people... -->
-                    </tbody>
-                </table>
             </div>
-            {{ $clients->links() }}
         </div>
-
-    </div>
     </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100 dark:bg-gray-900">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="min-h-screen">
-       <div x-data="{menuVisibility: false}">
-           <x-side-bar></x-side-bar>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
 
-           <div class="flex flex-col md:pl-64">
-                <x-navigation />
+    @stack('customer-scripts')
+</head>
+<body class="min-h-screen">
+<div x-data="{ menuVisibility: false }">
+    <x-side-bar></x-side-bar>
 
-               <main class=" flex-1">
-                   <div class="py-6">
-                       {{ $slot }}
-                   </div>
-               </main>
+    <div class="flex flex-col md:pl-64">
+        <x-navigation/>
 
-           </div>
-       </div>
-    </body>
+        <main class="flex-1">
+            <div class="py-6">
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
+</div>
+
+@livewireScripts
+</body>
 </html>
